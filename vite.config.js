7fe652e0e7af0a7970f7bf525436a5b136083b25
GAguileraMetabix <gaguilerametabix@gmail.com>
2024-11-06 14:19:06 -0300

    Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vercel from 'vite-plugin-vercel';
 
 export default defineConfig({
     base: process.env.APP_URL,
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
+    plugins: [vercel(),
     ],
 });
