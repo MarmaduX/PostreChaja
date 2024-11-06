96adc4586312c51c9729b74d75e140ec7a358e95
GAguileraMetabix <gaguilerametabix@gmail.com>
2024-11-06 15:38:34 -0300

    Vercel config

--- api/index.php
+++ api/index.php
@@ -1,11 +1,1 @@
-<?php
-
-require __DIR__ . "/../public/index.php";
-
-if ($_GET['type'] === 'css') {
-    header("Content-type: text/css; charset: UTF-8");
-    echo require __DIR__ . '/../public/css/' . basename($_GET['file']);
-} else if ($_GET['type'] === 'js') {
-    header('Content-Type: application/javascript; charset: UTF-8');
-    echo require __DIR__ . '/../public/js/' . basename($_GET['file']);
-}
+<?php require __DIR__ . '/../public/index.php';
