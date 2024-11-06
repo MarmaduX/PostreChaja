c135f385fecd874d02b24425a6cbe08d1f83fe5e
GAguileraMetabix <gaguilerametabix@gmail.com>
2024-11-06 15:40:00 -0300

    Vercel config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,7 +21,8 @@ return Application::configure(basePath: dirname(__DIR__))
             'role' => \App\Http\Middleware\RedirectIfNoRole::class,
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
-        ]);
+        ]);  
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
