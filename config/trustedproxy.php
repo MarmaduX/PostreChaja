378b1c2f1498b938527600676f5b0a7cc714f1c7
GAguileraMetabix <gaguilerametabix@gmail.com>
2024-11-06 13:17:23 -0300

    Truest Proxy

--- config/trustedproxy.php
+++ config/trustedproxy.php
@@ -1,2 +1,4 @@
-protected $proxies = '*';
-protected $headers = Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_HOST | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO | Request::HEADER_X_FORWARDED_PREFIX | Request::HEADER_X_FORWARDED_AWS_ELB;
+return [
+    'proxies' => '*', 
+    'proxies' => '*', // O especifica aquí la IP de Vercel si la conoces$headers = Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_HOST | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO | Request::HEADER_X_FORWARDED_PREFIX | Request::HEADER_X_FORWARDED_AWS_ELB,
+];
