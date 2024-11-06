f89d89d5212e9b2552bbcba4d08b22b6a5157f9d
GAguileraMetabix <gaguilerametabix@gmail.com>
2024-11-06 13:44:59 -0300

    Truest Prox

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    base: process.env.APP_URL + '/build/',
+    base: process.env.APP_URL,
     plugins: [
         laravel({
             input: [
