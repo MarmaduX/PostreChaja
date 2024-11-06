1d47c7c7c43653bf2c3f9adfb1a4c05e596e52ad
GAguileraMetabix <gaguilerametabix@gmail.com>
2024-11-06 14:51:22 -0300

    Vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,18 @@ import laravel from 'laravel-vite-plugin';
 import vercel from 'vite-plugin-vercel';
 
 export default defineConfig({
+    server: {
+        port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+    },
     base: process.env.APP_URL,
-    plugins: [vercel(),
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+        vercel(),
     ],
 });
